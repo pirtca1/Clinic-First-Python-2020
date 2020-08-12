57c1f78f63a2164f42f4603c4bd1a7b413e6b8a0
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-08-12 11:19:53 -0500

    Graphing works on Annaconda dist.

--- Algorithims/CoordinateSwap2.py
+++ Algorithims/CoordinateSwap2.py
@@ -1,22 +1,33 @@
-%matplotlib inline
 import matplotlib.pyplot as plt
-plt.style.use('seaborn=whitegrid')
+plt.style.use('seaborn-whitegrid')
 import numpy as np
-
+from scipy import stats
 
 
 def swapPositions(list, pos1, pos2): 
       
     list[pos1], list[pos2] = list[pos2], list[pos1] 
     m, b = np.polyfit(listXvalues, listYvalues, 1)
+    
     return list 
   
-listXvalues = [23,67,34,76,42,12,55,45,71,45,55,31] 
+listYvalues = [45,32,44,53,48,87,79,48,70,77,75,81] 
 pos1, pos2  = 1, 2
-  
-listYvalues = [0,1,2,3,4,5,6,7,8,9,10,11]
 
+listXvalues = [0,1,2,3,4,5,6,7,8,9,10,11]
 
-print(swapPositions(listXvalues, pos1-1, pos2-1)) 
+slope, intercept, r_value, p_value, std_err = stats.linregress(listXvalues, listYvalues)
+print("slope: %f    intercept: %f" % (slope, intercept))
+print("R-squared: %f" % r_value**2)
+print(swapPositions(listYvalues, pos1-1, pos2-1)) 
+
+while r_value < .8:
+    swapPositions(listYvalues, pos1-1, pos2-1)
+
+plt.plot(listXvalues,listYvalues)
+plt.yscale('linear')
+plt.title('Year_1')
+plt.grid(True)
+
 
 
