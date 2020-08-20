6769c866b17196519946c82b907d5db4fff3cd70
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-08-20 14:22:30 -0500

    Updated code, random works

--- Algorithims/QuarterMinMax.py
+++ Algorithims/QuarterMinMax.py
@@ -1,9 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from random import shuffle
 
 rotations1 = {"Inpt_Serv": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "Inpt_Serv": 4, "NICU": 4, "Neuro": 4, "Clinic": 33, "OB2": 2 }
 
+print("The original schedule for resident 1, year 1 is:" + str(rotations1))
+
+r1 = list(rotations1.items())
+random.shuffle(r1)
+rotations1 = dict(r1)
+
+print("the new schedule is:" + str (rotations1))
 keys = rotations1.keys()
 values = rotations1.values()
 plt.bar(keys, values)
