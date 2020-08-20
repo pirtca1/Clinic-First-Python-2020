6952fc6f255080372446e8a3d10405ad0c27715f
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-08-20 14:24:20 -0500

    Got rid of extra imports

--- Algorithims/QuarterMinMax.py
+++ Algorithims/QuarterMinMax.py
@@ -1,7 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import random
-from random import shuffle
 
 rotations1 = {"Inpt_Serv": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "Inpt_Serv": 4, "NICU": 4, "Neuro": 4, "Clinic": 33, "OB2": 2 }
 
