425d3dfae811f4ee27b15eb35d2a433fff702c83
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-09-02 18:03:24 -0500

    Mid edits

--- Algorithims/QuarterMinMax.py
+++ Algorithims/QuarterMinMax.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 import random
 
-rotations1 = {"Inpt_Serv": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "Inpt_Serv": 4, "NICU": 4, "Neuro": 4, "Clinic": 33, "OB2": 2 }
+rotations1 = {"InSrv": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "InSrv": 4, "NICU": 4, "Nuro": 4, "Clinic": 33, "OB2": 2 }
 
 print("The original schedule for resident 1, year 1 is:" + str(rotations1))
 
-r1 = list(rotations1.items())
-random.shuffle(r1)
-rotations1 = dict(r1)
 
-print("the new schedule is:" + str (rotations1))
-keys = rotations1.keys()
-values = rotations1.values()
+def rotationShuffle():
+    global r1
+    r1 = list(rotations1.items())
+    random.shuffle(r1)
+    return r1
+
+rotationShuffle()
+newRotations1 = dict(r1)
+print("the new schedule is:" + str (r1))
+keys = newRotations1.keys()
+values = newRotations1.values()
 plt.bar(keys, values)
