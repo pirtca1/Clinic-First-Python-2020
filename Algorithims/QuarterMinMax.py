389523fdbb86e6c2b5cfb61c597841b48dfdc681
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-09-02 18:04:59 -0500

    Current version

--- Algorithims/QuarterMinMax.py
+++ Algorithims/QuarterMinMax.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 13:39:55 2020
+
+@author: pirtca1
+"""
 import matplotlib.pyplot as plt
 import random
-
-rotations1 = {"InSrv": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "InSrv": 4, "NICU": 4, "Nuro": 4, "Clinic": 33, "OB2": 2 }
+import numpy as np
+#step 1 on chart
+rotations1 = {"InSrv1": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU":4 , "OB1": 8, "PedC": 4, "InSrv2": 4, "NICU": 4, "Nuro": 4, "Clinic": 33, "OB2": 2 }
 
 print("The original schedule for resident 1, year 1 is:" + str(rotations1))
 
-
+#step 2 on chart
 def rotationShuffle():
     global r1
     r1 = list(rotations1.items())
@@ -14,7 +22,53 @@ def rotationShuffle():
 
 rotationShuffle()
 newRotations1 = dict(r1)
-print("the new schedule is:" + str (r1))
+
+#third step 
+def quarterGet(lst, n):
+    global fourSplit
+    sessions = list(newRotations1.values())
+    fourSplit = np.array_split(sessions, 4)
+    return fourSplit, sessions
+class Quarter:
+
+    def quarterCreate():
+        q1 = [fourSplit[0]]
+        q2 = [fourSplit[1]]
+        q3 = [fourSplit[2]]
+        q4 = [fourSplit[3]]
+        return q1, q2,q3, q4
+    def quarterMaxMin(q1, q2, q3, q4):
+        for max in zip(q1, q2, q3, q4):
+                qMax = max
+        for min in zip(q1, q2, q3, q4):
+                qMin = min
+        quarterRange = qMax - qMin
+        return quarterRange
+        #fourth and fifth step
+    def quarterSwap(q1, q2, q3, q4, quarterRange):
+            if sum(q1) or sum(q2) or sum(q3) or sum(q4) >= quarterRange:
+                 for min in zip(q1, q2, q3, q4):
+                     min(random.choice(q1,q2,q3,q4)) == max(random.choice(q1, q2, q3, q4))
+            else:
+                    print(r1)
+                    rotationShuffle()
+                    quarterGet(sessions, 3)
+            
+    def ____call____(self):
+        self.quarterCreate()
+        self.quarterMaxMin()
+        self.quarterSwap()
+sessions = list(newRotations1.values())
+quarterGet(sessions, 3)
+quarter = Quarter
+quarter()
+quarter.quarterCreate()
+quarter.quarterMaxMin([fourSplit[0]], [fourSplit[1]], [fourSplit[2]], [fourSplit[3]])
+quarter.quarterSwap()
+
+
+#final step
+print("the new schedules are:" + str (r1))
 keys = newRotations1.keys()
 values = newRotations1.values()
 plt.bar(keys, values)
