ac3326b9a7e3ba8237d2893b752fa5a6e25a7f6e
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-09-30 16:58:56 -0500

    Steps plus a starting point

---
+++ Algorithims/LevelLoading.py
@@ -0,0 +1,8 @@
+ #It will need some work, the basic psuedo code is to have it pick a random starting point using random.randrange, multiply the quarter OR half values by 6, and then pick new starting points until they are within a certain range of eachother
+ 
+ i = qAll.index
+        for i in qAll(6):
+            randomSpot = random.randrange(0,len(qAll))
+            startCount = 1
+            print("This is startinf point #" + startCount)
+            startCount == startCount + 1
