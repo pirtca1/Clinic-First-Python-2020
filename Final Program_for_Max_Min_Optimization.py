abcf4b7aaa1fc81c8bbe6c9e93b4f41f324d48c1
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-10-23 09:36:36 -0500

    Final Algorithims

---
+++ Final Program_for_Max_Min_Optimization.py
@@ -0,0 +1,91 @@
+#Final_Program_for_Max_Min_Optimization
+# step1
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+# step 1 on chart
+rotations1 = {"InSrv1": 4, "Card1": 4, "Surg": 14, "Emed": 23, "ICU": 4, "OB1": 8, "PedC": 4, "InSrv2": 4, "NICU": 4,
+              "Neuro": 4, "Clinic": 33, "OB2": 2}
+
+print("The original schedule for resident 1, year 1 is:" + str(rotations1))
+
+# step 2 on chart
+r1 = list(rotations1.items())
+random.shuffle(r1)
+
+print(r1)
+
+newRotations1 = dict(rotations1)
+print("the new schedule is:" + str(r1))
+
+
+def rotationShuffle():
+    newRotations1 = dict(rotations1)
+    print("the new schedule is:" + str(newRotations1))  # otherwise str (r1)
+
+
+# third step
+def quarterGet(lst, n):
+    sessions = list(newRotations1.values())
+    fourSplit = np.array_split(lst, n)
+    return fourSplit
+    return sessions
+
+
+class Quarter:
+    def quarterCreate(self, fourSplit):
+        q1 = fourSplit[0]
+        q2 = fourSplit[1]
+        q3 = fourSplit[2]
+        q4 = fourSplit[3]
+        qAll = [q1, q2, q3, q4]
+        #return q1, q2, q3, q4, qAll
+        return qAll
+
+    def quarterMaxMin(self, q1, q2, q3, q4):
+        qMax = 0
+        qMin = 100
+        for q in (q1,q2,q3,q4):
+            for k in q:
+                val = int(k[1])
+                if qMax < val:
+                    qMax = val
+                if qMin > val:
+                    qMin = val
+
+        print(f'qMax = {qMax}')
+        print(f'qMin = {qMin}')
+        quarterRange = qMax - qMin
+        print(f'quarterRange = {quarterRange}')
+        return (qMax,qMin,quarterRange)
+        # fourth and fifth step
+
+    def sumCalc(self, q):
+        sum = 0
+        for k in q:
+            val = int(k[1])
+            sum = sum + val
+        return sum
+
+    def quarterSwap(self, q1, q2, q3, q4, quarterRange):
+        rotationShuffle()
+
+sessions1 = r1
+
+fourSplit = quarterGet(sessions1, 4)
+print('The foursplit value is :')
+
+quarter = Quarter()
+(q1,q2,q3,q4) = quarter.quarterCreate(fourSplit)
+print(quarter.sumCalc(q1), q1)
+print(q2)
+print(q3)
+print(q4)
+(qMax, qMin, quarterRange) = quarter.quarterMaxMin(q1,q2,q3,q4)
+quarter.quarterSwap( q1, q2, q3, q4, quarterRange)
+print("the new schedules are:" + str(r1))
+
+keys = q1,q2,q3,q4
+values = sessions
+plt.bar(keys, values)
---
+++ Goals/RandomAlgorithim.py
@@ -0,0 +1,36 @@
+import random
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+from scipy import stats
+
+
+def swapPositions(values): 
+    random.shuffle(values)
+    return values
+  
+#yearNumber = input("Type year number")
+
+valuesOne = [45,32,44,53,48,87,79,48,70,77,75,81] 
+ValuesTwo = [99,107,110	,108,113,96,123,100,114,119,110,100]
+valuesThree = [133,127,117,126,115,127,121,104,122,121,110,128]
+
+xvalues = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(xvalues, valuesOne)
+#print("slope: %f    intercept: %f" % (slope, intercept))
+#print("R-squared: %f" % r_value**2)
+print(swapPositions(valuesOne)) 
+
+
+"""while r_value < .8 :
+    swapPositions(valuesOne)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xvalues, valuesOne)
+    print(swapPositions(valuesOne))
+    print("R-squared: %f" % r_value**2)
+print(swapPositions(valuesOne))
+print("Done")
+    """
+plt.plot(xvalues,valuesOne)
+plt.yscale('linear')
+plt.title('Year_1')
+plt.grid(True)
